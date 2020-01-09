3dcc0c6dc68754ac8296369052aab6eca756e243
rabramini <rabramini@twilio.com>
2020-01-09 16:32:16 +0000

    Store code

---
+++ src/ExplodingComponent.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import Button from '@material-ui/core/Button';
+
+export class ExplodingComponent extends PureComponent {
+  render() {
+        return (
+            <Button>
+              Harmless button  ...or is it?
+            </Button>
+        )
+    }
+}
