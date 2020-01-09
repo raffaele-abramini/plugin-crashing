5233b77e334611386effa7e8d60dc84b204ead1b
rabramini <rabramini@twilio.com>
2020-01-09 16:55:59 +0000

    Store code

--- src/ReactBundledPlugin.js
+++ src/ReactBundledPlugin.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {ExplodingComponent} from "./ExplodingComponent";
 
 export default class DemoPlugin extends FlexPlugin {
-  name = 'DemoPlugin';
+  name = 'Crashing';
 
   init(flex, manager) {
     flex.CRMContainer.Content.add(<ExplodingComponent key="yeah" />);
